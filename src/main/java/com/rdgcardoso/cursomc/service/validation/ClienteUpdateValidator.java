5591ababaebbd020fc45b3a07b0aad41f55f8d78
Rodrigo Cardoso <rdgcardoso@outlook.com>
2020-04-15 00:32:31 -0300

    Validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/com/rdgcardoso/cursomc/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,55 @@
+package com.rdgcardoso.cursomc.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.rdgcardoso.cursomc.dto.ClienteDTO;
+import com.rdgcardoso.cursomc.model.Cliente;
+import com.rdgcardoso.cursomc.repository.ClienteRepository;
+import com.rdgcardoso.cursomc.resource.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		//EMAIL
+
+		//Pega o Map de variáveis de URI da requisição
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
